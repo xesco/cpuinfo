7d25b59b55c9a578abfda5e489ebe64a61c92a91
xesco <xescoo@gmail.com>
2019-04-16 23:51:20 +0200

    Refactor
    - Added another version of get_cpu_info
    - Added more generic set_headers
    - Return 500 in case of erro in the server
    - Pas port by parameter
    - Use ENTRYPOINT/CMD

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ EXPOSE 8080
 WORKDIR /opt/cpuinfo
 
 ADD cpuinfo.py .
-CMD ["/opt/cpuinfo/cpuinfo.py"]
+ENTRYPOINT ["/opt/cpuinfo/cpuinfo.py"]
+CMD ["8080"]
--- cpuinfo.py
+++ cpuinfo.py
@@ -13,6 +13,21 @@ from http.server import (
     BaseHTTPRequestHandler,
 )
 
+DEFAULT_PORT = 8080
+
+def get_totals(cpuinfo):
+    # We assume same CPU architecture for multi CPU systems
+    real     = len({cpuinfo[k]['physical_id'] for k in cpuinfo.keys()})
+    cores    = int(cpuinfo['0']['cpu_cores'])
+    total    = real*cores
+
+    # Hyperthreading support (added for completeness)
+    siblings = int(cpuinfo['0']['siblings'])
+    if cores != siblings:
+        cpuinfo['siblings'] = siblings
+        total *= siblings
+    return real, cores, total
+
 def get_cpu_info(file_path='/proc/cpuinfo'):
     """Get System's CPU/s specifications as a dict"""
     cpuinfo = {}
@@ -20,7 +35,7 @@ def get_cpu_info(file_path='/proc/cpuinfo'):
         for line in fd:
             try:
                 key, value = extract_values(line)
-                if key.lower() == 'processor':
+                if key == 'processor':
                     processor = value
                     cpuinfo[processor] = {} 
                 else:
@@ -33,21 +48,27 @@ def get_cpu_info(file_path='/proc/cpuinfo'):
                 # this should not happen
                 sys.stderr.write('Error: /proc/cpuinfo format not supported :(\n')
                 sys.exit(1)
-    # TOTALS
-    # We assume same CPU architecture for multi CPU systems
-    real     = len({cpuinfo[k]['physical_id'] for k in cpuinfo.keys()})
-    cores    = int(cpuinfo['0']['cpu_cores'])
-    total    = real*cores
+    cpuinfo['real'], cpuinfo['cores'], cpuinfo['total'] = get_totals(cpuinfo)
+    return cpuinfo
 
-    # Hyperthreading support (added for completeness)
-    siblings = int(cpuinfo['0']['siblings'])
-    if cores != siblings:
-        cpuinfo['siblings'] = siblings
-        total *= siblings
-
-    cpuinfo['real']  = real  # physical CPUs
-    cpuinfo['cores'] = cores # cores per CPU
-    cpuinfo['total'] = total # logical CPUs
+def get_cpu_info2(file_path='/proc/cpuinfo'):
+    """Get System's CPU/s specifications as a dict"""
+    cpuinfo = {}
+    procnum = str(0)
+    cpuinfo[procnum] = {}
+    with open(file_path) as fd:
+        for line in fd:
+            try:
+                key, value = extract_values(line)
+                if key != 'processor':
+                    cpuinfo[procnum][key] = value
+            except ValueError:
+                # empty line => next processor
+                procnum = str(int(procnum)+1)
+                cpuinfo[procnum] = {}
+    # extra space at the end of input => we need to delete empty processor
+    del cpuinfo[procnum]
+    cpuinfo['real'], cpuinfo['cores'], cpuinfo['total'] = get_totals(cpuinfo)
     return cpuinfo
 
 def extract_values(line):
@@ -59,7 +80,7 @@ def extract_values(line):
     # values as lists
     if key.lower() in ('flags', 'bugs'):
         value = value.split()
-    return key, value
+    return key.lower(), value
 
 def to_bytes(_dict, enc='utf-8'):
     """Convert dict to byte-json"""
@@ -70,7 +91,7 @@ def to_bytes(_dict, enc='utf-8'):
 class HTTPHandler(BaseHTTPRequestHandler):
     """Simple GET-only HTTP Server"""
 
-    cpuinfo = to_bytes(get_cpu_info())
+    cpuinfo = to_bytes(get_cpu_info2())
 
     def _set_headers(self, code):
         self.send_response(code)
@@ -86,14 +107,18 @@ class HTTPHandler(BaseHTTPRequestHandler):
             self.wfile.write(to_bytes({'error': str(ex)}))
             
         
-def run(server_class=HTTPServer, handler_class=HTTPHandler, port=8080):
+def run(port, server_class=HTTPServer, handler_class=HTTPHandler):
     """Main loop"""
 
     httpd = server_class(('', port), handler_class)
-    print('Server started!')
+    print(f'Server started at port {port}')
     httpd.serve_forever()
 
 if __name__ == '__main__':
-    run()
+    try:
+        port = int(sys.argv[1])
+    except:
+        port = DEFAULT_PORT
+    run(port)
 
 # END
